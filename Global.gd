0cc3e3d817fc38b6b392cd560a4c1210ba2531bf
JanisMancevics <splintercdo@gmail.com>
2018-04-22 06:12:46 +0300

    Full gameplay

--- Global.gd
+++ Global.gd
@@ -2,9 +2,18 @@ extends Node
 
 signal load_bullet_hell_scene
 signal load_novel_scene
+signal load_victory_scene
+signal load_game_over_scene
 
 export var bullet_hell_scene_path = "res://Scenes/World.tscn" # To know, what to load.
 export var novel_scene_path = "res://Scenes/Control.tscn" # To know, what to load.
+export var victory_scene_path = "res://Scenes/Victory.tscn" # To know, what to load.
+export var game_over_scene_path = "res://Scenes/GameOver.tscn" # To know, what to load.
+
+export var current_dialogue_index = 0 # To start dialogue from here.
+
+export var attempts = 3 # When there are no more attempts, load game over.
+
 var current_scene = null
 
 func _ready():
@@ -12,12 +21,20 @@ func _ready():
 	current_scene = root.get_child( root.get_child_count() -1 )
 	connect("load_bullet_hell_scene", self, "on_load_bullet_hell_scene")
 	connect("load_novel_scene", self, "on_load_novel_scene")
+	connect("load_victory_scene", self, "on_load_victory_scene")
+	connect("load_game_over_scene", self, "on_load_game_over_scene")
 
 func on_load_bullet_hell_scene():
 	goto_scene(bullet_hell_scene_path)
 
 func on_load_novel_scene():
 	goto_scene(novel_scene_path)
+
+func on_load_victory_scene():
+	goto_scene(victory_scene_path)
+
+func on_load_game_over_scene():
+	goto_scene(game_over_scene_path)
 
 func goto_scene(path):
 	call_deferred("_deferred_goto_scene", path)
--- Scenes/DialogueWrapper.gd
+++ Scenes/DialogueWrapper.gd
@@ -6,31 +6,33 @@ onready var button1 = get_node("Background/GirlText/VBoxContainer/Button1") # Fo
 onready var button2 = get_node("Background/GirlText/VBoxContainer/Button2") # For speed and convenience.
 onready var progress_bar = get_node("TextureProgress") # For speed and convenience.
 export (Array) var dialogue_options = [] # All the options of the player.
-export var current_dialogue_index = 0 # To start dialogue from here.
 var approximation_float = .0001 # Take floating point error into account.
 
 func _ready():
 	set_new_dialogue_state()
 
 func set_new_dialogue_state():
-	girl_text.text = dialogue_options[current_dialogue_index][4]
-	button0.text = dialogue_options[current_dialogue_index][1][0]
-	button1.text = dialogue_options[current_dialogue_index][2][0]
-	button2.text = dialogue_options[current_dialogue_index][3][0]
+	if dialogue_options[Global.current_dialogue_index][0] < 1.0 - approximation_float:
+		girl_text.text = dialogue_options[Global.current_dialogue_index][4]
+		button0.text = dialogue_options[Global.current_dialogue_index][1][0]
+		button1.text = dialogue_options[Global.current_dialogue_index][2][0]
+		button2.text = dialogue_options[Global.current_dialogue_index][3][0]
 
-	progress_bar.value = dialogue_options[current_dialogue_index][0]
+		progress_bar.value = dialogue_options[Global.current_dialogue_index][0]
 
-	if progress_bar.value < approximation_float:
-		Global.emit_signal("load_bullet_hell_scene")
+		if progress_bar.value < approximation_float:
+			Global.emit_signal("load_bullet_hell_scene")
+	else:
+		Global.emit_signal("load_victory_scene")
 
 func _on_Button0_button_down():
-	current_dialogue_index = dialogue_options[current_dialogue_index][1][1]
+	Global.current_dialogue_index = dialogue_options[Global.current_dialogue_index][1][1]
 	set_new_dialogue_state()
 
 func _on_Button1_button_down():
-	current_dialogue_index = dialogue_options[current_dialogue_index][2][1]
+	Global.current_dialogue_index = dialogue_options[Global.current_dialogue_index][2][1]
 	set_new_dialogue_state()
 
 func _on_Button2_button_down():
-	current_dialogue_index = dialogue_options[current_dialogue_index][3][1]
+	Global.current_dialogue_index = dialogue_options[Global.current_dialogue_index][3][1]
 	set_new_dialogue_state()
